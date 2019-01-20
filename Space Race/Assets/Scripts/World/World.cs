b04913b05822e5a39134efbf7701e20c577574f0
GnotAGnelf <46732648+GnotAGnelf@users.noreply.github.com>
2019-01-20 20:36:16 +1000

    Added Project

---
+++ Space Race/Assets/Scripts/World/World.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Intialising Test field
+        GameObject testField = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        testField.transform.localScale = new Vector3(10f, 1f, 10f);
+        Rigidbody testFieldPhysics = testField.AddComponent<Rigidbody>();
+        testFieldPhysics.useGravity = false;
+        MeshCollider testFieldCollider = testField.AddComponent<MeshCollider>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
